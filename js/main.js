3a4d8a8d6809c055ddbebc7f4af538a0be140a47
bjousselin <benjousselin@Bens-MacBook-Pro.local>
2014-11-19 19:45:42 -0500

    Styling
    -parallax added
    -nav fixed
    -media queries added
    -support for other browsers added
    -responsive grid

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var $win = $(window);
+var $introSection = $('.intro-section');
+
+$win.on('scroll', function () {
+  var scrollPos = $win.scrollTop();
+
+  $introSection.css('background-position', 'center ' + scrollPos /2.5 + 'px');
+});
